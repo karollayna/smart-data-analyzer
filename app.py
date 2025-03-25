184f52198d62644e3567aa579a821fd9db996abd
Karolina Scigocka <scigockakarolina@gmail.com>
2025-03-25 14:30:10 +0100

    updated version of streamlit app

--- app.py
+++ app.py
@@ -8,41 +8,76 @@ st.title('''
 Smart Data Analyzer :female-scientist: :male-scientist:
 :dart: This page makes it easy for you to create graphs from your data
 ''')
+st.markdown(
+    'Your data should be in the following **format**:', 
+    help='''
+📋 **Expected file format:** `.csv`  
+📋 **Required files and columns:**  
 
-uploaded_file = st.file_uploader('Add your data in file.csv', 
-                                 type='csv', 
-                                 accept_multiple_files=False, 
-                                 disabled=False, 
-                                 label_visibility="visible")
+**`data_photodynamic_therapy_cell_lines.csv`**
+- `cell_line_code`
+- `cell_line_name`
 
-user_column_names = st.text_input('Add your column names separated by comma')
+**`data_photodynamic_therapy_drugs.csv`**  
+- `drug_code`  
+- `drug_name`  
 
-if st.button('Done!'):
-    user_data = backend.update_user_data(uploaded_file, user_column_names)
-    complete_result = backend.calculate_average_std(user_data)
-    st.write(complete_result)
-    graph_avg, graph_std = backend.create_graph(complete_result)
+**`data_photodynamic_therapy_results.csv`**  
+- `experiment_id`  
+- `experiment_number`  
+- `cell_line_code`  
+- `treatment_time`  
+- `drug_code`  
+- `drug_concentration`  
+- `result_001 - result_012`
 
-    # https://plotly.com/python/distplot/
-    st.line_chart(graph_avg)
+**Note:** The number of `result` columns is fixed at 12. 
+'''
+)
+
+uploaded_files = backend.upload_user_files()
+
+if uploaded_files:
+    backend.validate_user_data(uploaded_files)
+# tutaj przyjrzec sie behaviorowi bo przycisk ma sie pojawiac tylko gdy dane sa valid    
+    st.button('Save your data :cloud:')
 
 
-if st.button('Save your result locally'):
-    uploaded_file_path = r"C:\Users\scigo\Desktop\portfolio\smart-data-analyzer\simple_data.csv"
-    user_data = backend.update_user_data(uploaded_file, user_column_names)
-    complete_result = backend.calculate_average_std(user_data)
-    backend.save_user_result(uploaded_file_path, complete_result)
-    # save_on_aws = st.button('Save your result on AWS S3 bucket')
-    st.success(f'Your result was saved')
+# backend.validate_user_data(uploaded_files=uploaded_files)
+# uploaded_files = st.file_uploader('Add your data in file.csv', 
+#                                  type='csv', 
+#                                  accept_multiple_files=True, 
+#                                  disabled=False, 
+#                                  label_visibility="visible")
 
-if st.button('Save on AWS s3 bucket'):
-    s3_secret_key = st.secrets["AWS_SECRET_ACCESS_KEY"]
-    key_id = st.secrets["AWS_ACCESS_KEY_ID"]
-    bucket_name = st.secrets["S3_BUCKET_NAME"]
-    region_name = st.secrets["AWS_DEFAULT_REGION"]
-    aws_connection = backend.AwsHandler(s3_secret_key, key_id, bucket_name, region_name)
+# backend.validate_user_data(uploaded_files=uploaded_files)
+
+# if st.button('Done!'):
+#     user_data = backend.update_user_data(uploaded_file, user_column_names)
+#     complete_result = backend.calculate_average_std(user_data)
+#     st.write(complete_result)
+#     graph_avg, graph_std = backend.create_graph(complete_result)
+
+#     # https://plotly.com/python/distplot/
+#     st.line_chart(graph_avg)
+
+
+# if st.button('Save your result locally'):
+#     uploaded_file_path = r"C:\Users\scigo\Desktop\portfolio\smart-data-analyzer\simple_data.csv"
+#     user_data = backend.update_user_data(uploaded_file, user_column_names)
+#     complete_result = backend.calculate_average_std(user_data)
+#     backend.save_user_result(uploaded_file_path, complete_result)
+#     # save_on_aws = st.button('Save your result on AWS S3 bucket')
+#     st.success(f'Your result was saved')
+
+# if st.button('Save on AWS s3 bucket'):
+#     s3_secret_key = st.secrets["AWS_SECRET_ACCESS_KEY"]
+#     key_id = st.secrets["AWS_ACCESS_KEY_ID"]
+#     bucket_name = st.secrets["S3_BUCKET_NAME"]
+#     region_name = st.secrets["AWS_DEFAULT_REGION"]
+#     aws_connection = backend.AwsHandler(s3_secret_key, key_id, bucket_name, region_name)
     
-    with st.spinner("Przesyłanie..."):
-        aws_connection.upload(uploaded_file.name)
-        st.success(f"Plik {uploaded_file.name} zapisany w S3!")
-        st.write('good job!!!')
+#     with st.spinner("Przesyłanie..."):
+#         aws_connection.upload(uploaded_file.name)
+#         st.success(f"Plik {uploaded_file.name} zapisany w S3!")
+#         st.write('good job!!!')
--- backend.py
+++ backend.py
@@ -3,102 +3,151 @@ import streamlit as st
 import os
 import boto3
 
-def update_user_data(uploaded_file_path, user_column_names):
-        
-    if uploaded_file_path is not None:
-        user_data = pd.read_csv(uploaded_file_path, header=0, index_col=0)
-        if user_column_names:
-            column_names = [str(name) for name in user_column_names.split(",")] #.split(",")
-            if len(column_names) == len(user_data.columns):
-                user_data.columns = column_names
+def upload_user_files():
+    # upload user files
+    uploaded_files = st.file_uploader(
+        'Add your data in file.csv', 
+        type='csv', 
+        accept_multiple_files=True
+        )
+    if uploaded_files:
+        return uploaded_files
 
-    return user_data
+def validate_user_data(uploaded_files):
+    # Define expected files and columns
+    expected_files = {
+        'data_photodynamic_therapy_cell_lines.csv': [
+            'cell_line_code',
+            'cell_line_name'
+        ],
+        'data_photodynamic_therapy_drugs.csv': [
+            'drug_code',
+            'drug_name'
+        ],
+        'data_photodynamic_therapy_results.csv': [
+            'experiment_id', 'experiment_number', 'cell_line_code',
+            'treatment_time', 'drug_code', 'drug_concentration',
+            'result_001', 'result_002','result_003',
+            'result_004', 'result_005', 'result_006',
+            'result_007','result_008','result_009',
+            'result_010', 'result_011', 'result_012'
+        ]
+    }
 
-
-def calculate_average_std(my_obj):
-    avg = my_obj.mean(axis='rows').round(2)
-    st_deviation = my_obj.std(axis='rows').round(2)
-
-    avg_df = avg.to_frame().T
-    st_deviation_df = st_deviation.to_frame().T
-
-    avg_df.index = ['avg']
-    st_deviation_df.index = ['std']
-        
-    complete_result = pd.concat([my_obj, avg_df, st_deviation_df])
+    unexpected_files = [file.name for file in uploaded_files if file.name not in expected_files.keys()]
     
-    return complete_result
+    # Handle unexpected files
+    if unexpected_files:
+        st.error(f':x: Unexpected files: {unexpected_files}\n'
+                 f'Expected files: {list(expected_files.keys())}')
+        return
 
-
-def create_graph(complete_result):
-    chart_df = complete_result.loc[['avg', 'std']].T
-    avg_data = chart_df['avg']
-    std_data = chart_df['std']
-    return avg_data, std_data
-
-def save_user_result(uploaded_file_path, complete_result):
-    current_directory = os.path.dirname(uploaded_file_path)
-    destination_folder_name = "data_analysis"
-    destination_folder_path = os.path.join(current_directory, destination_folder_name)
-    if not os.path.exists(destination_folder_path):
-        os.makedirs(destination_folder_path)
-        print(f"Folder '{destination_folder_name}' został utworzony w katalogu: {current_directory}")
-    else:
-        print(f"Folder '{destination_folder_name}' już istnieje w katalogu: {current_directory}")
-    
-    csv_file_name = os.path.splitext(os.path.basename(uploaded_file_path))[0] + '.csv'
-    csv_file_path = os.path.join(destination_folder_path, csv_file_name)
-
-    complete_result.to_csv(csv_file_path, index=False)
-    
-    print(f"Wynik zapisano do pliku: {csv_file_path}")
-
-
-# uploaded_file_path = r"C:\Users\scigo\Desktop\portfolio\smart-data-analyzer\simple_data.csv"
-# user_column_names = ['skg1','2','3','4','5','6','7','8','9','10','11','12']
-# my_obj = update_user_data(uploaded_file_path, user_column_names)
-# # print(my_obj)
-    
-
-# complete_result = calculate_average_std(my_obj)
-# print(complete_result)
-
-
-# save_user_result(uploaded_file_path, complete_result)
-# print(pd.read_csv(r"C:\Users\scigo\Desktop\portfolio\smart-data-analyzer\data_analysis\simple_data.csv"))
-
-
-class AwsHandler():
-    def __init__(self, s3_secret_key, key_id, bucket_name, region_name):
-        self.s3_secret_key = s3_secret_key
-        self.key_id = key_id
-        self.bucket_name = bucket_name
-        self.region_name = region_name
-        self.s3_user = boto3.client('s3', 
-                               aws_access_key_id=self.key_id, 
-                               aws_secret_access_key=self.s3_secret_key,
-                               region_name=self.region_name)
-        
-    def upload(self, file_name):
-        f = open(file_name, 'rb')
-        self.s3_user.upload_fileobj(f, self.bucket_name, f'{file_name}')
-        print(f'{file_name} uploaded')
-
-    def download_file(self, file_name):
-        self.s3_user.download_file(self.bucket_name, self.file_name, f'downloaded_{file_name}')
-        print(f'File {file_name} downloaded')
-
-    def check_if_file_exist(self, file_name):
-        file_list = self.list_files()
-        return file_name in [obj['Key'] for obj in file_list]
-
-    def list_files(self, pattern=None):
-        if pattern is None:
-            response = self.s3_user.list_objects_v2(Bucket=self.bucket_name)
+    # Read and validate user data
+    for uploaded_file in uploaded_files:
+        user_data = pd.read_csv(uploaded_file)
+        expected_columns = expected_files.get(uploaded_file.name, [])
+        if user_data.empty:
+            st.error(f':x: File "{uploaded_file.name}" is empty.')
+            continue
+        if user_data.columns.tolist() != expected_columns:
+            st.error(f''':x: File "{uploaded_file.name}" has incorrect columns.\n
+                    Expected: {expected_columns}\n Found: {user_data.columns.tolist()}''')
         else:
-            response = self.s3_user.list_objects_v2(Bucket=self.bucket_name, Prefix=pattern)
-        file_names = [obj for obj in response["Contents"]]
-        print(file_names)
-        return file_names
+            st.success(f':white_check_mark: File "{uploaded_file.name}" is valid.')
 
-    print('object created')
+# def update_user_data(uploaded_file_path):
+        
+#     if uploaded_file_path is not None:
+#         user_data = pd.read_csv(uploaded_file_path, header=0, index_col=0)
+#         st.write(user_data)
+
+#     return user_data
+
+
+# def calculate_average_std(my_obj):
+#     avg = my_obj.mean(axis='rows').round(2)
+#     st_deviation = my_obj.std(axis='rows').round(2)
+
+#     avg_df = avg.to_frame().T
+#     st_deviation_df = st_deviation.to_frame().T
+
+#     avg_df.index = ['avg']
+#     st_deviation_df.index = ['std']
+        
+#     complete_result = pd.concat([my_obj, avg_df, st_deviation_df])
+    
+#     return complete_result
+
+
+# def create_graph(complete_result):
+#     chart_df = complete_result.loc[['avg', 'std']].T
+#     avg_data = chart_df['avg']
+#     std_data = chart_df['std']
+#     return avg_data, std_data
+
+# def save_user_result(uploaded_file_path, complete_result):
+#     current_directory = os.path.dirname(uploaded_file_path)
+#     destination_folder_name = "data_analysis"
+#     destination_folder_path = os.path.join(current_directory, destination_folder_name)
+#     if not os.path.exists(destination_folder_path):
+#         os.makedirs(destination_folder_path)
+#         print(f"Folder '{destination_folder_name}' został utworzony w katalogu: {current_directory}")
+#     else:
+#         print(f"Folder '{destination_folder_name}' już istnieje w katalogu: {current_directory}")
+    
+#     csv_file_name = os.path.splitext(os.path.basename(uploaded_file_path))[0] + '.csv'
+#     csv_file_path = os.path.join(destination_folder_path, csv_file_name)
+
+#     complete_result.to_csv(csv_file_path, index=False)
+    
+#     print(f"Wynik zapisano do pliku: {csv_file_path}")
+
+
+# # uploaded_file_path = r"C:\Users\scigo\Desktop\portfolio\smart-data-analyzer\simple_data.csv"
+# # user_column_names = ['skg1','2','3','4','5','6','7','8','9','10','11','12']
+# # my_obj = update_user_data(uploaded_file_path, user_column_names)
+# # # print(my_obj)
+    
+
+# # complete_result = calculate_average_std(my_obj)
+# # print(complete_result)
+
+
+# # save_user_result(uploaded_file_path, complete_result)
+# # print(pd.read_csv(r"C:\Users\scigo\Desktop\portfolio\smart-data-analyzer\data_analysis\simple_data.csv"))
+
+
+# class AwsHandler():
+#     def __init__(self, s3_secret_key, key_id, bucket_name, region_name):
+#         self.s3_secret_key = s3_secret_key
+#         self.key_id = key_id
+#         self.bucket_name = bucket_name
+#         self.region_name = region_name
+#         self.s3_user = boto3.client('s3', 
+#                                aws_access_key_id=self.key_id, 
+#                                aws_secret_access_key=self.s3_secret_key,
+#                                region_name=self.region_name)
+        
+#     def upload(self, file_name):
+#         f = open(file_name, 'rb')
+#         self.s3_user.upload_fileobj(f, self.bucket_name, f'{file_name}')
+#         print(f'{file_name} uploaded')
+
+#     def download_file(self, file_name):
+#         self.s3_user.download_file(self.bucket_name, self.file_name, f'downloaded_{file_name}')
+#         print(f'File {file_name} downloaded')
+
+#     def check_if_file_exist(self, file_name):
+#         file_list = self.list_files()
+#         return file_name in [obj['Key'] for obj in file_list]
+
+#     def list_files(self, pattern=None):
+#         if pattern is None:
+#             response = self.s3_user.list_objects_v2(Bucket=self.bucket_name)
+#         else:
+#             response = self.s3_user.list_objects_v2(Bucket=self.bucket_name, Prefix=pattern)
+#         file_names = [obj for obj in response["Contents"]]
+#         print(file_names)
+#         return file_names
+
+#     print('object created')
