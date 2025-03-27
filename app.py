ca311bebc06b46f1f639f0bcdd6aac26ab78dab3
Karolina Scigocka <scigockakarolina@gmail.com>
2025-03-27 15:15:28 +0100

    add logic for loading data to snowflake from aws s3 bucket

--- app.py
+++ app.py
@@ -4,44 +4,45 @@ import backend
 import plotly.graph_objects as go
 from st_files_connection import FilesConnection
 
-
-st.title('''
+st.title(
+    """
 Smart Data Analyzer :female-scientist: :male-scientist:
 :dart: This page makes it easy for you to create graphs from your data
-''')
+"""
+)
 st.markdown(
-    'Your data should be in the following **format**:', 
-    help='''
-📋 **Expected file format:** `.csv`  
-📋 **Required files and columns:**  
+    "Your data should be in the following **format**:",
+    help="""
+📋 **Expected file format:** `.csv`
+📋 **Required files and columns:**
 
 **`data_photodynamic_therapy_cell_lines.csv`**
 - `cell_line_code`
 - `cell_line_name`
 
-**`data_photodynamic_therapy_drugs.csv`**  
-- `drug_code`  
-- `drug_name`  
+**`data_photodynamic_therapy_drugs.csv`**
+- `drug_code`
+- `drug_name`
 
-**`data_photodynamic_therapy_results.csv`**  
-- `experiment_id`  
-- `experiment_number`  
-- `cell_line_code`  
-- `treatment_time`  
-- `drug_code`  
-- `drug_concentration`  
+**`data_photodynamic_therapy_results.csv`**
+- `experiment_id`
+- `experiment_number`
+- `cell_line_code`
+- `treatment_time`
+- `drug_code`
+- `drug_concentration`
 - `result_001 - result_012`
 
-**Note:** The number of `result` columns is fixed at 12. 
-'''
+**Note:** The number of `result` columns is fixed at 12.
+""",
 )
 
 uploaded_files = backend.upload_user_files()
 
 if uploaded_files:
-    valid_files = backend.validate_user_data(uploaded_files)    
+    valid_files = backend.validate_user_data(uploaded_files)
     if valid_files:
-        if st.button('Save your data :cloud:'):
-            with st.spinner('Uploading your data to the cloud...'):
+        if st.button("Save your data :cloud:"):
+            with st.spinner("Uploading your data to the cloud..."):
                 backend.upload_files_to_s3(valid_files)
-                st.success(':white_check_mark: Your data has been saved to the cloud.')
+                st.success(":white_check_mark: Your data has been saved to the cloud.")
--- backend.py
+++ backend.py
@@ -3,44 +3,55 @@ import streamlit as st
 import boto3
 from datetime import datetime
 
+
 def upload_user_files():
     # upload user files
     uploaded_files = st.file_uploader(
-        'Add your data in file.csv', 
-        type='csv', 
-        accept_multiple_files=True
-        )
+        "Add your data in file.csv", type="csv", accept_multiple_files=True
+    )
     return uploaded_files
+
 
 def validate_user_data(uploaded_files):
     # Define expected files and columns
     expected_files = {
-        'data_photodynamic_therapy_cell_lines.csv': [
-            'cell_line_code',
-            'cell_line_name'
+        "data_photodynamic_therapy_cell_lines.csv": [
+            "cell_line_code",
+            "cell_line_name",
         ],
-        'data_photodynamic_therapy_drugs.csv': [
-            'drug_code',
-            'drug_name'
+        "data_photodynamic_therapy_drugs.csv": ["drug_code", "drug_name"],
+        "data_photodynamic_therapy_results.csv": [
+            "experiment_id",
+            "experiment_number",
+            "cell_line_code",
+            "treatment_time",
+            "drug_code",
+            "drug_concentration",
+            "result_001",
+            "result_002",
+            "result_003",
+            "result_004",
+            "result_005",
+            "result_006",
+            "result_007",
+            "result_008",
+            "result_009",
+            "result_010",
+            "result_011",
+            "result_012",
         ],
-        'data_photodynamic_therapy_results.csv': [
-            'experiment_id', 'experiment_number', 'cell_line_code',
-            'treatment_time', 'drug_code', 'drug_concentration',
-            'result_001', 'result_002','result_003',
-            'result_004', 'result_005', 'result_006',
-            'result_007','result_008','result_009',
-            'result_010', 'result_011', 'result_012'
-        ]
     }
 
     valid_files = []
     unexpected_files = []
-    
+
     for uploaded_file in uploaded_files:
         if uploaded_file.name not in expected_files:
             unexpected_files.append(uploaded_file.name)
-            st.error(f''':x: Unexpected files: {unexpected_files}\n
-                     Expected files: {list(expected_files.keys())}''')
+            st.error(
+                f""":x: Unexpected files: {unexpected_files}\n
+                     Expected files: {list(expected_files.keys())}"""
+            )
         else:
             user_data = pd.read_csv(uploaded_file)
             expected_columns = expected_files[uploaded_file.name]
@@ -48,31 +59,34 @@ def validate_user_data(uploaded_files):
                 st.error(f':x: File "{uploaded_file.name}" is empty.')
                 continue
             if list(user_data.columns) != expected_columns:
-                st.error(f''':x: File "{uploaded_file.name}" has incorrect columns.\n
-                         Expected: {expected_columns}\n Found: {list(user_data.columns)}''')
+                st.error(
+                    f""":x: File "{uploaded_file.name}" has incorrect columns.\n
+                         Expected: {expected_columns}\n Found: {list(user_data.columns)}"""
+                )
                 continue
 
             valid_files.append((uploaded_file.name, uploaded_file.getvalue()))
             st.success(f':white_check_mark: File "{uploaded_file.name}" is valid.')
     return valid_files
 
+
 def upload_files_to_s3(valid_files):
-    
+
     s3_secret_key = st.secrets["AWS_SECRET_ACCESS_KEY"]
     key_id = st.secrets["AWS_ACCESS_KEY_ID"]
     bucket_name = st.secrets["S3_BUCKET_NAME"]
     region_name = st.secrets["AWS_DEFAULT_REGION"]
-    s3_user = boto3.client('s3', 
-                            aws_access_key_id=key_id, 
-                            aws_secret_access_key=s3_secret_key,
-                            region_name=region_name)
+    s3_user = boto3.client(
+        "s3",
+        aws_access_key_id=key_id,
+        aws_secret_access_key=s3_secret_key,
+        region_name=region_name,
+    )
 
     for file_name, file_content in valid_files:
         current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
         unique_file_name = f"{current_time}_{file_name}"
-        s3_user.put_object(
-            Bucket = bucket_name,
-            Key = unique_file_name,
-            Body = file_content
-            )            
-        st.success(f':white_check_mark: File "{unique_file_name}" has been uploaded to the cloud.')
+        s3_user.put_object(Bucket=bucket_name, Key=unique_file_name, Body=file_content)
+        st.success(
+            f':white_check_mark: File "{unique_file_name}" has been uploaded to the cloud.'
+        )
