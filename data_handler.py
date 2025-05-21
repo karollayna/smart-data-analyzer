2c4ccc2642da6abb67866ee2e6803d3ac60850fd
Karolina Scigocka <scigockakarolina@gmail.com>
2025-05-21 15:04:15 +0200

    refactor: clean up data processing logic

--- data_handler.py
+++ data_handler.py
@@ -103,152 +103,154 @@ class DataHandler:
             st.success(f':white_check_mark: File "{uploaded_file.name}" is valid.')
     
         return self.valid_files
-
-    def fetch_experiment_data(self, data, experiment_number):
-        """
-        Fetches experiment data for a specific experiment number.
-
-        Parameters:
-        ----------
-        data : DataFrame
-            The DataFrame containing experiment data.
-        experiment_number : int
-            The number of the experiment to fetch data for.
-
-        Returns:
-        -------
-        experiment_data : DataFrame
-            A DataFrame containing data for the specified experiment number.
-        """
-        experiment_data = pd.DataFrame(data)
-        experiment_data = experiment_data[experiment_data['EXPERIMENT_NUMBER'] == experiment_number]
-        
-        return experiment_data
-
-    def analyze_experiment_data(self, experiment_data):
-        """
-        Analyzes the experiment data by calculating mean and standard deviation of result columns.
-
-        Parameters:
-        ----------
-        experiment_data : DataFrame
-            The DataFrame containing experiment data to analyze.
-
-        Returns:
-        -------
-        experiment_data : DataFrame
-            The DataFrame with added 'MEAN' and 'STD' columns.
-        control_means : DataFrame
-            A DataFrame containing mean values for control groups.
-        full_experiment_data : DataFrame
-            The DataFrame with merged experiment data and control means.
-        """
-        results_columns = [col for col in experiment_data.columns if col.startswith('RESULT_')]
-        
-        experiment_data['MEAN'] = experiment_data[results_columns].mean(axis=1, skipna=True).round(2)
-        experiment_data['STD'] = experiment_data[results_columns].std(axis=1, skipna=True).round(2)
-        
-        controls = experiment_data[(experiment_data['TREATMENT_TIME'] == 0) & (experiment_data['DRUG_CONCENTRATION'] == 0)]
-        control_means = controls.groupby(['DRUG_NAME', 'CELL_LINE_NAME'])['MEAN'].mean().reset_index()
-
-        full_experiment_data = experiment_data.merge(control_means, on=['DRUG_NAME', 'CELL_LINE_NAME'], suffixes=('', '_CONTROL'))
-
-        return experiment_data, control_means, full_experiment_data
     
-    def calculate_survival(self, full_experiment_data):
-        """
-        Calculates the survival rate based on mean values and control means.
 
-        Parameters:
-        ----------
-        full_experiment_data : DataFrame
-            The DataFrame containing merged experiment data and control means.
 
-        Returns:
-        -------
-        full_experiment_data : DataFrame
-            The DataFrame with an added 'SURVIVAL_RATE' column.
-        """
+    # def fetch_experiment_data(self, data, experiment_number):
+    #     """
+    #     Fetches experiment data for a specific experiment number.
+
+    #     Parameters:
+    #     ----------
+    #     data : DataFrame
+    #         The DataFrame containing experiment data.
+    #     experiment_number : int
+    #         The number of the experiment to fetch data for.
+
+    #     Returns:
+    #     -------
+    #     experiment_data : DataFrame
+    #         A DataFrame containing data for the specified experiment number.
+    #     """
+    #     experiment_data = pd.DataFrame(data)
+    #     experiment_data = experiment_data[experiment_data['EXPERIMENT_NUMBER'] == experiment_number]
         
-        def calculate_survival_row(row):
-            if row['MEAN_CONTROL'] == 0:
-                survival = 0
-            else:
-                survival = ((row['MEAN'] / row['MEAN_CONTROL']) * 100)
-            
-            survival = round(survival, 2)
-            return survival
+    #     return experiment_data
 
-        full_experiment_data['SURVIVAL_RATE'] = full_experiment_data.apply(calculate_survival_row, axis=1)
+    # def analyze_experiment_data(self, experiment_data):
+    #     """
+    #     Analyzes the experiment data by calculating mean and standard deviation of result columns.
 
-        return full_experiment_data
+    #     Parameters:
+    #     ----------
+    #     experiment_data : DataFrame
+    #         The DataFrame containing experiment data to analyze.
+
+    #     Returns:
+    #     -------
+    #     experiment_data : DataFrame
+    #         The DataFrame with added 'MEAN' and 'STD' columns.
+    #     control_means : DataFrame
+    #         A DataFrame containing mean values for control groups.
+    #     full_experiment_data : DataFrame
+    #         The DataFrame with merged experiment data and control means.
+    #     """
+    #     results_columns = [col for col in experiment_data.columns if col.startswith('RESULT_')]
+        
+    #     experiment_data['MEAN'] = experiment_data[results_columns].mean(axis=1, skipna=True).round(2)
+    #     experiment_data['STD'] = experiment_data[results_columns].std(axis=1, skipna=True).round(2)
+        
+    #     controls = experiment_data[(experiment_data['TREATMENT_TIME'] == 0) & (experiment_data['DRUG_CONCENTRATION'] == 0)]
+    #     control_means = controls.groupby(['DRUG_NAME', 'CELL_LINE_NAME'])['MEAN'].mean().reset_index()
+
+    #     full_experiment_data = experiment_data.merge(control_means, on=['DRUG_NAME', 'CELL_LINE_NAME'], suffixes=('', '_CONTROL'))
+
+    #     return experiment_data, control_means, full_experiment_data
     
-    def create_plots(self, user_result, filter_type, selected_value, x_axis, y_axis, treatment_times):
-        """
-        Creates a list of scatter plots based on the filtered data.
+    # def calculate_survival(self, full_experiment_data):
+    #     """
+    #     Calculates the survival rate based on mean values and control means.
 
-        Parameters:
-        - **user_result** (DataFrame): The DataFrame containing the user's results.
-        - **filter_type** (str): The type of filter to apply. Can be either "Drugs" or "CELL_LINE_NAME".
-        - **selected_value** (str): The value to filter by.
-        - **x_axis** (str): The column name for the x-axis.
-        - **y_axis** (str): The column name for the y-axis.
-        - **treatment_times** (list): A list of treatment times to create plots for. Each time should be in a format that can be sorted numerically (e.g., "0 min", "5 min", etc.).
+    #     Parameters:
+    #     ----------
+    #     full_experiment_data : DataFrame
+    #         The DataFrame containing merged experiment data and control means.
 
-        Returns:
-        - **figures** (list): A list of Plotly figures, each representing a scatter plot for a specific treatment time.
-
-        Notes:
-        - The function first filters the data based on the specified filter type and value.
-        - It then sorts the treatment times in ascending order before creating the plots.
-        - If 'CELL_LINE_NAME' or 'DRUG_NAME' columns are present in the filtered data, they are used for coloring the points in the scatter plots.
-        """
-        def format_axis_title(axis_name):
-            return axis_name.replace("_", " ").lower().capitalize()
+    #     Returns:
+    #     -------
+    #     full_experiment_data : DataFrame
+    #         The DataFrame with an added 'SURVIVAL_RATE' column.
+    #     """
         
-        if filter_type == "Drugs":
-            # Filter by drug name
-            filtered_df = user_result[user_result['DRUG_NAME'] == selected_value] 
-        else:
-            # Filter by cell line name
-            filtered_df = user_result[user_result['CELL_LINE_NAME'] == selected_value]
+    #     def calculate_survival_row(row):
+    #         if row['MEAN_CONTROL'] == 0:
+    #             survival = 0
+    #         else:
+    #             survival = ((row['MEAN'] / row['MEAN_CONTROL']) * 100)
+            
+    #         survival = round(survival, 2)
+    #         return survival
 
-        treatment_times = list(treatment_times)
-        treatment_times.sort(key=lambda x: int(x.split()[0]) if isinstance(x, str) else x)
+    #     full_experiment_data['SURVIVAL_RATE'] = full_experiment_data.apply(calculate_survival_row, axis=1)
 
-        figures = []
-        for time in treatment_times:
-            # Filter the data for the treatment time
-            df_subset = filtered_df[filtered_df['TREATMENT_TIME'] == time]
-            
-            if 'CELL_LINE_NAME' in df_subset.columns:
-                color_col = 'CELL_LINE_NAME'
-            elif 'DRUG_NAME' in df_subset.columns:
-                color_col = 'DRUG_NAME'
-            else:
-                color_col = None
+    #     return full_experiment_data
+    
+    # def create_plots(self, user_result, filter_type, selected_value, x_axis, y_axis, treatment_times):
+    #     """
+    #     Creates a list of scatter plots based on the filtered data.
 
-            x_axis_title = format_axis_title(x_axis)
-            y_axis_title = format_axis_title(y_axis)
-            
-            fig = px.scatter(
-                df_subset,
-                x=x_axis,
-                y=y_axis,
-                color=color_col if color_col else None,
-                hover_data=['DRUG_NAME', 'CELL_LINE_NAME'],
-                title=f"{x_axis_title} vs {y_axis_title} for {selected_value} at {time} min"
-            )
-            
-            fig.update_layout(
-                xaxis_title=x_axis_title,
-                yaxis_title=y_axis_title,
-                showlegend=True if color_col else False,
-                xaxis=dict(showgrid=True),
-                yaxis=dict(showgrid=True)
-            )
-            
-            figures.append(fig)
+    #     Parameters:
+    #     - **user_result** (DataFrame): The DataFrame containing the user's results.
+    #     - **filter_type** (str): The type of filter to apply. Can be either "Drugs" or "CELL_LINE_NAME".
+    #     - **selected_value** (str): The value to filter by.
+    #     - **x_axis** (str): The column name for the x-axis.
+    #     - **y_axis** (str): The column name for the y-axis.
+    #     - **treatment_times** (list): A list of treatment times to create plots for. Each time should be in a format that can be sorted numerically (e.g., "0 min", "5 min", etc.).
+
+    #     Returns:
+    #     - **figures** (list): A list of Plotly figures, each representing a scatter plot for a specific treatment time.
+
+    #     Notes:
+    #     - The function first filters the data based on the specified filter type and value.
+    #     - It then sorts the treatment times in ascending order before creating the plots.
+    #     - If 'CELL_LINE_NAME' or 'DRUG_NAME' columns are present in the filtered data, they are used for coloring the points in the scatter plots.
+    #     """
+    #     def format_axis_title(axis_name):
+    #         return axis_name.replace("_", " ").lower().capitalize()
         
-        return figures
+    #     if filter_type == "Drugs":
+    #         # Filter by drug name
+    #         filtered_df = user_result[user_result['DRUG_NAME'] == selected_value] 
+    #     else:
+    #         # Filter by cell line name
+    #         filtered_df = user_result[user_result['CELL_LINE_NAME'] == selected_value]
+
+    #     treatment_times = list(treatment_times)
+    #     treatment_times.sort(key=lambda x: int(x.split()[0]) if isinstance(x, str) else x)
+
+    #     figures = []
+    #     for time in treatment_times:
+    #         # Filter the data for the treatment time
+    #         df_subset = filtered_df[filtered_df['TREATMENT_TIME'] == time]
+            
+    #         if 'CELL_LINE_NAME' in df_subset.columns:
+    #             color_col = 'CELL_LINE_NAME'
+    #         elif 'DRUG_NAME' in df_subset.columns:
+    #             color_col = 'DRUG_NAME'
+    #         else:
+    #             color_col = None
+
+    #         x_axis_title = format_axis_title(x_axis)
+    #         y_axis_title = format_axis_title(y_axis)
+            
+    #         fig = px.scatter(
+    #             df_subset,
+    #             x=x_axis,
+    #             y=y_axis,
+    #             color=color_col if color_col else None,
+    #             hover_data=['DRUG_NAME', 'CELL_LINE_NAME'],
+    #             title=f"{x_axis_title} vs {y_axis_title} for {selected_value} at {time} min"
+    #         )
+            
+    #         fig.update_layout(
+    #             xaxis_title=x_axis_title,
+    #             yaxis_title=y_axis_title,
+    #             showlegend=True if color_col else False,
+    #             xaxis=dict(showgrid=True),
+    #             yaxis=dict(showgrid=True)
+    #         )
+            
+    #         figures.append(fig)
+        
+    #     return figures
 
--- pages/3_Data_Processing.py
+++ pages/3_Data_Processing.py
@@ -6,10 +6,18 @@ st.set_page_config(
 
 st.title("Data Processing")
 
-if st.session_state["data_uploaded"] and not st.session_state['data_updated']:
-    ##TODO: find a faster way to do this
-    st.write("**Processing your data...**")
-    st.write("placeholder")
-    st.session_state['data_updated'] = True
+if st.session_state['user_id']:
+    if st.session_state["data_uploaded"] and not st.session_state['data_updated']:
+        st.write("**Processing your data...**")
+
+        
+        
+
+
+
+
+
+
+        st.session_state['data_updated'] = True
 
    
